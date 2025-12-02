b9720ef61cff10da29228ee12d90f2268623ee10
harryfritz <mhfritz@gmail.com>
2025-12-02 13:44:52 +0100

    Puzzle solved but answer not accepted.

--- Day2/day2.cpp
+++ Day2/day2.cpp
@@ -13,10 +13,36 @@
 #include <string>
 using namespace std;
 
-int isIDvalid(int id){
+int digits(long int id){
+    //Tells how many digits an id has
+    int digits = 0;
+    while (id) {
+        id /= 10;
+        digits++;
+    }
+    return digits;
+}
+
+
+long long isIDvalid(long long id){
     //Checks if ID is valid.
     //If yes, return id
     //If no, return 0
+        
+    int numDig = digits(id);
+    if(numDig%2 != 0) return 0; //exclude all IDs with odd number of digits
+    
+    int lowerHalf, higherHalf;
+    int splitter = 1;
+    for(int i = 0; i < numDig/2; i++){
+        splitter *= 10;
+    }
+    lowerHalf = id % splitter;
+    higherHalf = id / splitter; 
+
+    if(lowerHalf == higherHalf){
+        return id;
+    }
 
     return 0;
 }
@@ -30,7 +56,7 @@ int main() {
         exit(1);   
     }
 
-    long int answer = 0;
+    long long answer = 0;
 
     string input;
     getline(file, input);
@@ -38,24 +64,29 @@ int main() {
     int nextChar = 0;
     int commaPos = input.find(",",nextChar);
     string nextRange;
-    long int rangeStart, rangeEnd;
+    long long rangeStart, rangeEnd;
+    
     while(true){
         
         nextRange = input.substr(nextChar,commaPos-nextChar);
-        cout << "(" + to_string(commaPos) + ") " + nextRange + " ";
+        cout << "\n(" + to_string(commaPos) + ") " + nextRange + " ";
         
         rangeStart = stoll(nextRange.substr(0, nextRange.find("-")));
         rangeEnd = stoll(nextRange.substr(nextRange.find("-") + 1, nextRange.size() - nextRange.find("-")));
-        for(long int id = rangeStart; id <= rangeEnd; id++){
+        for(long long id = rangeStart; id <= rangeEnd; id++){
             answer += isIDvalid(id);
+            if(isIDvalid(id) != 0){
+                cout << "\ninvalid id:" + to_string(id) + " new answer:" + to_string(answer);
+            } 
         }
-        cout << "new answer: " + to_string(answer) + "\n";
+        //cout << "new answer: " + to_string(answer) + "\n";
 
         nextChar = commaPos + 1;
         if(commaPos == std::string::npos){
             break;
         }
         commaPos = input.find(",",nextChar);
+
     }
 
     file.close();
