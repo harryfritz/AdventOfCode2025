a65eccbe928a1b286f31b324feb65ea44aa2ab79
harryfritz <mhfritz@gmail.com>
2025-12-02 02:40:15 +0100

    Day 1 Parts 1 and 2 working

--- Day1/day1.cpp
+++ Day1/day1.cpp
@@ -17,6 +17,9 @@ using namespace std;
 int main() {
   
     ifstream file;
+    
+    
+    // Puzzle 1
     file.open("input.txt");
 
     if (!file) {
@@ -60,6 +63,75 @@ int main() {
 
     cout << "Password: " + to_string(password);
 
+    file.close();
+
+    
+    
+    
+    cout << "\n\n\n\n";
+    // Puzzle 2
+    file.open("input.txt");
+
+    if (!file) {
+        cerr << "Unable to open file input.txt";
+        exit(1);   
+    }
+
+    arrowPos = 50;
+    password = 0;
+    rotation = 0;
+
+    int column = 0;
+
+    while (getline(file, input))
+    {
+        cout << "Pt2-" + input + "-New Pos:";
+        
+        
+        
+        if(input[0] == 'L'){
+            rotation = -stoi(input.erase(0,1));
+            while(arrowPos + rotation < 0){
+                rotation += 100;
+                password++;
+            }
+            if(arrowPos == 0){
+                password--;
+            }     
+
+            arrowPos += rotation;
+            if(arrowPos == 0){
+                password++;
+            }   
+        } else if(input[0] == 'R'){
+            rotation = stoi(input.erase(0,1));
+            while(arrowPos + rotation > 99){
+                rotation -= 100;
+                if(arrowPos + rotation != 0) {
+                    password++;
+                }
+            }
+
+            arrowPos += rotation;
+            if(arrowPos == 0){
+                password++;
+            }  
+        } else {
+            cerr << "Reading error!";
+        }        
+        
+        cout << to_string(arrowPos) + " PWD: " + to_string(password);
+        if(column <= 3) {
+            column++;
+            cout << "\t";
+        } else {
+            column = 0;
+            cout << "\n";
+        }
+
+    }
+
+    cout << "New Password: " + to_string(password);
 
     file.close();
 
