cfe349ba96ebb13cbc1ab8de75f7ae94db925648
harryfritz <mhfritz@gmail.com>
2025-12-02 10:31:51 +0100

    Day 2 First Commit

---
+++ day2.cpp
@@ -0,0 +1,34 @@
+/////////////////////////////////////////////////
+//
+// Advent of Code 2025
+// Day 2 - 
+// Author: Fritz, M.H.
+//
+//
+/////////////////////////////////////////////////
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+int main() {
+  
+    ifstream file;
+    file.open("input.txt");
+    if (!file) {
+        cerr << "Unable to open file input.txt";
+        exit(1);   
+    }
+
+    string input;
+    while (getline(file, input))
+    {
+        cout << input + "\n";
+    }
+
+    file.close();
+    return 0;
+}
