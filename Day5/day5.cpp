0a0a6a85c009ec6314f6b9f46a1cdc5181319a8c
harryfritz <mhfritz@gmail.com>
2025-12-06 02:58:08 +0100

    Day 5 Parts one and two solved

--- Day5/day5.cpp
+++ Day5/day5.cpp
@@ -25,38 +25,73 @@ int main() {
 
     string input;
     vector<vector <long long>> ranges;
-    long long id;
-    bool isFresh;
-    long long freshCount = 0;
+    int rangesSize = 0;
+    long long idCount = 0;
     
-
     for(int i = 0; getline(file, input); i++){
         if(input == "") break;
         ranges.push_back({stoll(input.substr(0, input.find("-"))), stoll(input.substr(input.find("-") + 1, input.size() - input.find("-")))});
-    }    
+    }
     
-    while(getline(file, input)){
-        cout << "\n" + input;
+    
+    // PART 2
+    while(rangesSize != ranges.size()){
+        cout << "\nRanges Size:" << ranges.size();
+        rangesSize = ranges.size();
 
-        isFresh = false;
-        id = stoll(input);
         for(int i = 0; i < ranges.size(); i++){
-            if(id >= ranges[i][0] && id <= ranges[i][1]){
-                isFresh = true;
-                break;
+            for(int j = 0; j < ranges.size(); j++){
+                if(j != i) {
+                    if((ranges[i][1] <= ranges[j][1] && ranges[i][1] >= ranges[j][0])){
+                        if(ranges[i][0] < ranges[j][0]) {
+                            ranges[j][0] = ranges[i][0];
+                        }
+                        ranges.erase(ranges.begin() + i);
+                    }
+                }
             }
-        }
-
-        if(isFresh){
-            cout << " fresh";
-            freshCount++;
-        } else {
-            cout << " spoiled";
         }
 
     }
 
-    cout << "\nFresh Count: " << freshCount;
+    for(int i = 0; i < ranges.size(); i++){
+        idCount += ranges[i][1] - ranges[i][0] + 1;
+    }
+    cout << "\nIDs Count: " << idCount;
+
+    // // Part 1
+
+    // int id;
+    // bool isFresh;
+    // int freshCount = 0;
+
+    // for(int i = 0; getline(file, input); i++){
+    //     if(input == "") break;
+    //     ranges.push_back({stoll(input.substr(0, input.find("-"))), stoll(input.substr(input.find("-") + 1, input.size() - input.find("-")))});
+    // }    
+    
+    // while(getline(file, input)){
+    //     cout << "\n" + input;
+
+    //     isFresh = false;
+    //     id = stoll(input);
+    //     for(int i = 0; i < ranges.size(); i++){
+    //         if(id >= ranges[i][0] && id <= ranges[i][1]){
+    //             isFresh = true;
+    //             break;
+    //         }
+    //     }
+
+    //     if(isFresh){
+    //         cout << " fresh";
+    //         freshCount++;
+    //     } else {
+    //         cout << " spoiled";
+    //     }
+
+    // }
+
+    // cout << "\nFresh Count: " << freshCount;
     
     file.close();
     return 0;
