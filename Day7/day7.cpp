5357e14383a92df70f7ff180756176b2b5d9f03d
harryfritz <mhfritz@gmail.com>
2025-12-07 18:26:46 +0100

    Day 7 part one solved

--- Day7/day7.cpp
+++ Day7/day7.cpp
@@ -10,25 +10,46 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
-
 
 int main() {
   
     ifstream file;
     file.open("input.txt");
-    if (!file) {
-        cerr << "Unable to open file input.txt";
-        exit(1);   
-    }
 
     string input;
-
-    while(getline(file, input)){
-        cout << "\n" + input;
+    vector<vector <char>> manifold;
+    for(int i = 0; getline(file, input); i++){
+        manifold.push_back({});
+        for(int j = 0; j < input.size(); j++){
+            manifold[i].push_back(input[j]);
+        }
     }
-    
+
+    int totalSplits = 0;
+    for(int i = 0; i < manifold.size(); i++){
+        cout << "\n";
+        for(int j = 0; j < manifold[i].size(); j++){
+            if(i == 0) {
+                if(manifold[i][j] == 'S') {
+                    manifold[i+1][j] = '|';
+                }
+            } else if (i != 1) {
+                if(manifold[i][j] == '^' && manifold[i-1][j] == '|'){
+                    manifold[i][j-1] = '|';
+                    manifold[i][j+1] = '|';
+                    totalSplits++;
+                } else if(manifold[i-1][j] == '|') {
+                    manifold[i][j] = '|';
+                }
+            }
+            cout << manifold[i][j];
+        }
+    }
+
+    cout << "\nTotal Splits: " << totalSplits;
     
     file.close();
     return 0;
