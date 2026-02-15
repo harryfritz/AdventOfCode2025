d89488cbc4f3395446593bb6743cb3b418fe321f
harryfritz <mhfritz@gmail.com>
2026-02-15 21:23:57 +0100

    Day9 clean version

--- Day10/day10.cpp
+++ Day10/day10.cpp
@@ -11,12 +11,39 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <math.h>
 
 using namespace std; 
 
 vector <vector <char> > indicators;
 
-vector <vector <vector <int> > > buttons;
+vector <vector <int> > buttons;
+
+vector <int> buttonCombinations;
+
+
+
+int intPow(int base, int exponent){
+    int result = 1;
+    for(int i = 0; i < exponent; i++){
+        result *= base;
+    }
+    return result;
+}
+
+bool isButtonSeqCorrect(int machine, int buttonResult){    
+    for(int i = 0; i < indicators[machine].size(); i++){
+        int digit = ((buttonResult / intPow(10,i)) % 10);
+        if(digit % 2 == 0 && indicators[machine][i] == '#' || digit % 2 == 1 && indicators[machine][i] == '.') {
+            return false;
+        }
+    }
+    return true;
+}
+
+void combineButtons(int machine, int k){
+    
+}
 
 int main() {
   
@@ -26,20 +53,39 @@ int main() {
     
     for(int i = 0; getline(file, input); i++){
         cout << "\n" + input;
+        
         string indicator = input.substr(1, input.find("]") - 1);
         indicators.push_back({});
         for(char c : indicator){
             indicators[i].push_back(c);
         }
 
-        string button = input.substr(input.find("]"), input.find("{") - 1);
+        string button = input.substr(input.find("]") + 1, input.find("{") - input.find("]") - 1);
         buttons.push_back({});
-        for(char c : button){
-            if(c == '('){
+        int j = -1;
+        for(int c = 0; c < button.size(); c++){
+            if(button[c] == '('){
                 buttons[i].push_back({});
+                j++;
+            }
+            if(button[c] >= '0' && button[c] <= '9'){
+                buttons[i][j] += intPow(10,button[c] - 48);
             }
         }
     }
+
+    
+    
+    
+    int machine = 0;
+    for(int pushes = 1; pushes <= indicators[machine].size(); pushes++){
+        
+
+        
+        
+        if(isButtonSeqCorrect(machine, 1));
+    }
+
     
     
     file.close();
