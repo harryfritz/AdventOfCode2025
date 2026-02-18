eee31bd0cb02c028be596358982029722abd6b8c
harryfritz <mhfritz@gmail.com>
2026-02-18 02:25:05 +0100

    Day10 part2 brute force try

--- Day10/day10_pt2.cpp
+++ Day10/day10_pt2.cpp
@@ -12,101 +12,30 @@
 #include <string>
 #include <vector>
 #include <math.h>
+#include <algorithm>
 
 using namespace std; 
 
-bool isButtonCombCorrect(vector <char>& indicators, vector <int>& btnComb){    
-    for(int i = 0; i < indicators.size(); i++){
-        if(btnComb[i] % 2 == 0 && indicators[i] == '#' || btnComb[i] % 2 == 1 && indicators[i] == '.') {
+bool isBtnCombCorrect(vector <int>& btnComb, vector <vector <int>>& buttons, vector <int>& joltages){
+
+    vector <int> joltagesTry;
+    for(int i = 0; i < joltages.size(); i++){
+        joltagesTry.push_back(0);
+    }
+
+    for(int i = 0; i < btnComb.size(); i++){
+        for(int k = 0; k < joltagesTry.size(); k++){
+            joltagesTry[k] += btnComb[i] * buttons[i][k];
+        }
+    }
+
+    for(int i = 0; i < joltagesTry.size(); i++){
+        if(joltagesTry[i] != joltages[i]){
             return false;
         }
     }
-    return true;
-}
-
-void combinationUtil(vector <vector <int>>& machine, int ind, int r, 
-                        vector <vector <vector <int>>>& combinations, vector <vector <int>>& dataArr) {
-    int n = machine.size();
-
-    if(ind == 0){
-        dataArr.clear();
-        combinations.clear();
-    }
     
-    if (dataArr.size() == r) {
-        combinations.push_back(dataArr);
-        return;
-    }
-
-    for(int i = ind; i < n; i++) {
-        dataArr.push_back(machine[i]);
-        combinationUtil(machine, i + 1, r, combinations, dataArr);
-        dataArr.pop_back();
-    }
-}
-
-vector <vector <int>> buttonCombinations(vector <vector <int>>& machine, int r){
-    vector <vector <vector <int>>> combinations;
-    vector <vector <int>> dataArr;
-    vector <vector <int>> btnComb;
-
-    combinationUtil(machine, 0, r, combinations, dataArr);
-
-    for(int i = 0; i < combinations.size(); i++){
-        btnComb.push_back({});
-        for(int k = 0; k < combinations[i][0].size(); k++){
-            int sum = 0;
-            for(int j = 0; j < combinations[i].size(); j++){
-                sum += combinations[i][j][k];
-            }
-            btnComb[i].push_back(sum);
-        }
-    }
-
-    return btnComb;
-}
-
-int determinant(vector <vector <int>>& matrix){
-
-    if(matrix.size() == 2){
-        return matrix[0][0] * matrix[0][0] - matrix[0][1] * matrix[1][0];
-    }
-
-    int det = 0;
-    for(int j = 0; j < matrix[0].size(); j++){
-        vector <vector <int>> minor = matrix;
-
-        minor.erase(minor.begin());
-        for(int i = 0; i < minor.size(); i++){
-            minor[i].erase(minor[i].begin() + j);
-        }
-
-        if(j % 2 == 0){
-            det += matrix[0][j] * determinant(minor);
-        } else {
-            det -= matrix[0][j] * determinant(minor);
-        }
-    }
-
-    return det;
-
-}
-
-vector <int> sysSolution(vector <vector <int>>& A, vector <int>& B){
-    vector <int> X;
-
-    int detA = determinant(A);
-
-    for(int x = 0; x < B.size(); x++){
-        vector <vector <int>> Ab = A;
-        for(int i = 0; i < B.size(); i++){
-            Ab[i][x] = B[i];
-        }
-        X.push_back(determinant(Ab) / detA);
-    }
-
-    return X;
-
+    return true;
 }
 
 int main() {
@@ -147,51 +76,55 @@ int main() {
         }
     }
 
-    vector <vector <vector <int>>> btnComb;
-    vector <vector <int>> dataArr;
-
     int answer = 0;
     for(int machine = 0; machine < buttons.size(); machine++){
-        int buttonsPressed = 100000;
-        combinationUtil(buttons[machine], 0, joltages[machine].size(), btnComb, dataArr);
-        for(vector <vector <int>> base : btnComb){
-            if(determinant(base) != 0){
-                vector <int> solution = sysSolution(base, joltages[machine]);
-                int sum = 0;
-                for(int press : solution){
-                    if(press < 0){
-                        sum = 0;
-                        break;
-                    }
-                    sum += press;
-                }
-                if(sum < buttonsPressed){
-                    buttonsPressed = sum;
-                }
+        
+        int presses = 0;
+        for(int i = 0; i < joltages[machine].size(); i++){
+            if(joltages[machine][i] > presses){
+                presses = joltages[machine][i];
             }
         }
-        answer += buttonsPressed;
+
+        int maxPresses = presses * joltages[machine].size();
+        bool found = false;
+        while(presses < maxPresses && !found){
+            vector <int> btnPresses;
+            
+            for(int i = 0; i < presses; i++){
+                btnPresses.push_back(1);
+            }
+
+            for(int i = 0; i < buttons[machine].size() - 1; i++){
+                btnPresses.push_back(0);
+            }
+
+            sort(btnPresses.begin(), btnPresses.end());
+            do {
+                int acum = 0;
+                vector <int> btnComb = {0};
+                for (int i = 0; i < btnPresses.size(); i++){
+                    if(btnPresses[i] == 0){
+                        btnComb.push_back(acum);
+                        acum = 0;
+                    } else if(btnPresses[i] == 1){
+                        acum++;
+                    }
+                }
+
+                if(isBtnCombCorrect(btnComb, buttons[machine], joltages[machine])){
+                    found = true;
+                }
+
+            } while (next_permutation(btnPresses.begin(), btnPresses.end()) && !found);
+
+            answer += presses;
+            presses++;
+        }
+
     }
+
     cout << "\n\nAnswer: " << answer;
-    
-    // int answer = 0;
-    // for(int machine = 0; machine < buttons.size(); machine++){
-    //     bool foundCombination = false;
-    //     for(int presses = 1; presses <= buttons[machine].size() && !foundCombination; presses++){            
-
-    //         for(vector <int> combination : buttonCombinations(buttons[machine], presses)){
-    //             if(isButtonCombCorrect(indicators[machine], combination)){
-    //                 cout << "\nMachine " << machine << ", " << presses << " button presses";
-    //                 foundCombination = true;
-    //                 answer += presses;
-    //                 break;
-    //             }
-    //         }
-
-    //     }
-    // }
-    // cout << "\n\nAnswer: " << answer;
-    
 
     file.close();
     return 0;
