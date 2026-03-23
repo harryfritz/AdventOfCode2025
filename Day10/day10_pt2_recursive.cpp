e9737d7c01707d30a20ca357a88757147c7e7044
harryfritz <mhfritz@gmail.com>
2026-03-23 16:46:40 +0100

    Refactor button combination and input handling

--- Day10/day10_pt2_recursive.cpp
+++ Day10/day10_pt2_recursive.cpp
@@ -13,55 +13,122 @@
 #include <vector>
 #include <math.h>
 #include <algorithm>
-#include <bits/stdc++.h>
 
-using namespace std; 
+using namespace std;
+
+#define INF 1000000
 
 void combinationUtil(vector <vector <int>>& buttons, int ind, int r, 
                         vector <vector <vector <int>>>& combinations, vector <vector <int>>& dataArr) {
     int n = buttons.size();
 
-    if(ind == 0){
+    if(ind == 0) {
         dataArr.clear();
         combinations.clear();
     }
     
-    if (dataArr.size() == r) {
+    if(dataArr.size() == r) {
         combinations.push_back(dataArr);
         return;
     }
 
     for(int i = ind; i < n; i++) {
         dataArr.push_back(buttons[i]);
-        combinationUtil(machine, i + 1, r, combinations, dataArr);
+        combinationUtil(buttons, i + 1, r, combinations, dataArr);
         dataArr.pop_back();
     }
 }
 
-vector <string> split(const string& s, char delim) {
+vector <string> split(const string& text, char delim) {
     vector <string> tokens;
-    string token;
-    stringstream ss(s);
     
-    while (getline(ss, token, delim)){
-        tokens.push_back(token);
+    int nextDelim = 0, cursor = 0;
+    while(nextDelim >= 0) {
+        nextDelim = text.find(delim, cursor + 1);
+        tokens.push_back(text.substr(cursor, nextDelim - cursor));
+        cursor = nextDelim + 1;
     }
 
     return tokens;
 }
 
-int minBtnPresses(vector <vector <int>>& buttons, vector <int> joltages){
+int minBtnPresses(vector <vector <int>>& buttons, vector <int>& joltages) {
 
-    vector <vector <vector <int>>> combinations;
-    vector <vector <int>> dataArr;
+    bool zero = true;
+    for(int i = 0; i < joltages.size() && zero; i++) {
+        if(joltages[i] != 0) {
+            zero = false;
+        }
+    }
+    if(zero) {
+        return 0;
+    }
 
-    combinationUtil(buttons, 0, )
+    vector <int> btnPresses;
+    vector <vector <vector <int>>> allCombinations;
+
+    // Populate allCombinations with every possible button combination from 1 to buttons.size()
+    for(int i = 1; i <= buttons.size(); i++) {
+        vector <vector <vector <int>>> combinations;
+        vector <vector <int>> dataArr;
+        combinationUtil(buttons, 0, i, combinations, dataArr);
+        for(vector <vector <int>> combination : combinations) {
+            allCombinations.push_back(combination);
+        }
+    }
+
+    // Goes through every combination checking which patterns match joltages
+    for(int btnComb = 0; btnComb < allCombinations.size(); btnComb++) {
+        
+        // pattern adds all buttons from a given combination. Ex.: (1,3) (1,2) = [0101] [0110] = [0211]
+        vector <int> pattern = allCombinations[btnComb][0];
+        for(int btn = 1; btn < allCombinations[btnComb].size(); btn++) {
+            for(int i = 0; i < pattern.size(); i++) {
+                pattern[i] += allCombinations[btnComb][btn][i];
+            }
+        } 
+
+        // Check if pattern matches parity with joltages. If yes, moves forward
+        bool validComb = true;
+        for(int i = 0; i < pattern.size() && validComb; i++) {
+            if((pattern[i] % 2) != (joltages[i] % 2) || pattern[i] > joltages[i]){
+                validComb = false;
+            }
+        }
+
+        if(validComb){
+            vector <int> joltagesCache = joltages;
+            for(int i = 0; i < joltagesCache.size(); i++) {
+                joltagesCache[i] -= pattern[i];
+                joltagesCache[i] /= 2;
+                if(joltagesCache[i] < 0) {
+                    validComb = false;
+                }
+            }
+
+            if(validComb) {
+                int min = minBtnPresses(buttons, joltagesCache);
+                if(min != -1){
+                    btnPresses.push_back(2*min + allCombinations[btnComb].size());
+                }
+            }
+            
+        }
+
+    }
     
+    if(btnPresses.size() == 0) {
+        return -1;
+    } else {
+        int minPresses = btnPresses[0];
+        for(int presses : btnPresses) {
+            if(presses < minPresses) {
+                minPresses = presses;
+            }
+        }
+        return minPresses;
+    }
     
-    
-    
-    
-    return 1000000;
 }
 
 int main() {
@@ -75,7 +142,7 @@ int main() {
 
     int answer = 0;
 
-    while(getline(file, input)){        
+    for(int line = 0; getline(file, input); line++) {        
         
         vector <string> tokens = split(input, ' ');
         tokens.erase(tokens.begin());
@@ -84,7 +151,7 @@ int main() {
         string inner = tokens.back().substr(1, tokens.back().size() - 2);
         vector <string> joltagesStr = split(inner, ',');
 
-        for(string j : joltagesStr){
+        for(string j : joltagesStr) {
             joltages.push_back(stoi(j));
         }
         
@@ -97,14 +164,19 @@ int main() {
             string inner = token.substr(1, token.size() - 2);
             vector<string> buttonsStr = split(inner, ',');
 
+            for(int j : joltages) button.push_back(0);
             for (string t : buttonsStr) {
-                button.push_back(stoi(t));
+                button[stoi(t)] = 1;
             }
             buttons.push_back(button);
         }
 
-        answer += minBtnPresses(buttons, joltages);
-        
+        if(line == 2) {
+            int presses = minBtnPresses(buttons, joltages);
+            answer += presses;
+            cout << "\nLine " << line << " - " << presses << " Presses";
+        }
+ 
     }
 
     
