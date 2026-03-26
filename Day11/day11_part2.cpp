8e913a5f9a629571e2f61f6c4f1af55ab7fd3a25
harryfritz <mhfritz@gmail.com>
2026-03-27 00:23:59 +0100

    Day11 part2

--- Day11/day11_part2.cpp
+++ Day11/day11_part2.cpp
@@ -15,9 +15,8 @@
 #include <algorithm>
 #include <list>
 #include <map>
-#include <chrono>
+#include <queue>
 
-using namespace std::chrono;
 using namespace std;
 
 class Graph {
@@ -52,24 +51,56 @@ class Graph {
 
         }
 
+        void bfs(int start, vector<vector<int>>& graph, int vertices){
+            vector<bool> visited(vertices, false); // Track visited vertices
+            queue<int> q;
+            visited[start] = true; //Mark starting vertex as visited
+            q.push(start); // added to queue
+
+            while(!q.empty()){
+                int current = q.front(); // Get the next vertex to explore
+                q.pop(); // Remove it from queue
+                cout << current << " ";
+                // Visit all the neighbours of the current vertex
+                for(int neighbour : graph[current]){
+                    if(!visited[neighbour]){ // if not visited
+                        visited[neighbour] = true; // mark as visited
+                        q.push(neighbour); // Add to the queue
+                    }
+                }
+            }
+        }
+
         void dfsRestricted(string node, string dest, map<string, bool> &visited, int &count, vector <string> &mandatoryNodes){
 
             if(node == dest) {
                 if(mandatoryNodes.empty()){
                     count++;
+                    cout << "\r" << count << "   ";
                 }
                 return;
             }
 
             visited.at(node) = true;
+            
+            bool mandNode = false;
+            for(int i = 0; i < mandatoryNodes.size(); i++){
+                if(node == mandatoryNodes[i]) {
+                    mandNode = true;
+                    mandatoryNodes.erase(mandatoryNodes.begin() + i);
+                }
+            }
 
             for(string neighbor : adjList.at(node)) {
                 if(!visited.at(neighbor)) {
-                    dfs(neighbor, dest, visited, count);
+                    dfsRestricted(neighbor, dest, visited, count, mandatoryNodes);
                 }
             }
 
             visited.at(node) = false;
+            if(mandNode){
+                mandatoryNodes.push_back(node);
+            }
 
         }
 
@@ -101,7 +132,10 @@ class Graph {
             
             
             cout << "\n" << count;
+            
             dfs(source, destination, visited, count);
+
+            // bfs(source, destination, visited, count);
 
             return count;
         }
@@ -111,6 +145,8 @@ class Graph {
             visited.insert({"out", false});
             for (auto node : adjList) visited.insert({node.first, false});
             int count = 0;
+
+            cout << "\n" << count;
             
             dfsRestricted(source, destination, visited, count, mandatoryNodes);
 
@@ -133,12 +169,47 @@ vector <string> split(const string& text, char delim) {
     return tokens;
 }
 
+void dfs(int node, int dest, vector<vector<int>> &graph, vector<bool> &visited, int &count) {
+    
+    if (node == dest) {
+        count++;
+        cout << "\r" << count << "   ";
+        return;
+    }
+
+    visited[node] = true;
+
+    for (int neighbor : graph[node]) {
+        if (!visited[neighbor]) {
+            dfs(neighbor, dest, graph, visited, count);
+        }
+    }
+
+    visited[node] = false;
+}
+
+int countPaths(int n, vector<vector<int>> &edgeList, int source, int destination) {
+
+    vector<vector<int>> graph(n + 1); 
+    for (auto &edge : edgeList) {
+        int u = edge[0];
+        int v = edge[1];
+        graph[u].push_back(v);
+    }
+
+    vector<bool> visited(n + 1, false);
+    int count = 0;
+
+    dfs(source, destination, graph, visited, count);
+
+    return count;
+}
+
 int main() {
-  
-    auto start = high_resolution_clock::now();
     
     ifstream file;
-    file.open("input.txt");
+    string fileName = "input.txt";
+    file.open(fileName);
     string input;
 
     Graph devices;
@@ -153,14 +224,48 @@ int main() {
         }
  
     }
-
-    int answer = devices.possiblePaths("fft","out");
+    
+    // int answer = devices.possiblePaths("svr","out");
+    int answer = devices.possibleRestrictedPaths("svr", "out", {"dac","fft"});
     cout << "\n\nAnswer: " << answer;
+
+    // map <string, int> nodeDictionary;
+    // int graphSize = 0;
+    // while(getline(file, input)) {        
+        
+    //     vector <string> device = split(input, ' ');
+    //     device[0].pop_back(); // Remove ':'
+
+    //     nodeDictionary.insert({device[0], graphSize + 1});
+        
+    //     graphSize++;
+    // }
+    
+    // // Include node "out"
+    // nodeDictionary.insert({"out", graphSize + 1});
+    // graphSize++; 
+
+    // file.close();
+    // file.open(fileName);
+    // vector<vector<int>> edgeList;
+    // for(int line = 0; getline(file, input); line++) {        
+        
+    //     vector <string> device = split(input, ' ');
+    //     device[0].pop_back(); // Remove ':'
+        
+    //     for(int output = 1; output < device.size(); output++) {
+    //         edgeList.push_back({line + 1, nodeDictionary.at(device[output])});
+    //     }
+ 
+    // }
+
+    // int source = nodeDictionary.at("svr");
+    // int destination = nodeDictionary.at("out");
+
+    // int answer = countPaths(graphSize, edgeList, source, destination);
+    // cout << "\n\nAnswer: " << answer;
     
     file.close();
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    cout << "\nTime: " << duration.count() << endl;
     return 0;
     
 }
