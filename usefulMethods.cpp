d36a92e77d604c773974330a24e1538b1c1c9c85
harryfritz <mhfritz@gmail.com>
2026-04-13 22:33:40 +0200

    quicksort implemented on usefulMethods

--- usefulMethods.cpp
+++ usefulMethods.cpp
@@ -628,6 +628,28 @@ void DfsIterative(int start, int finish, const std::vector<std::vector<int>>& gr
     }
 }
 
+
+int partition(vector<int> &vec, int low, int high) {
+    int pivot = vec[high];
+    int i = (low - 1);
+    for (int j = low; j <= high - 1; j++) {
+        if (vec[j] <= pivot) {
+            i++;
+            swap(vec[i], vec[j]);
+        }
+    }
+    swap(vec[i + 1], vec[high]);
+    return (i + 1);
+}
+
+void quickSort(vector<int> &vec, int low, int high) {
+    if (low < high) {
+        int pi = partition(vec, low, high);
+        quickSort(vec, low, pi - 1);
+        quickSort(vec, pi + 1, high);
+    }
+}
+
 int main() {
   
     ifstream file;
@@ -640,5 +662,6 @@ int main() {
     }
     
     file.close();
+
     return 0;
 }
