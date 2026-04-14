168a6a61b357b31f598d366f7709fe909ce645dd
harryfritz <mhfritz@gmail.com>
2026-04-14 22:05:18 +0200

    2024 Day3 first try

---
+++ 2024/Day3/day3_part1.cpp
@@ -0,0 +1,42 @@
+/////////////////////////////////////////////////
+//
+// Advent of Code 2024
+// Day 3 - Part one
+// Author: Fritz, M.H.
+//
+//
+/////////////////////////////////////////////////
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <regex>
+
+using namespace std;
+
+int main() {
+  
+    ifstream file;
+    file.open("example.txt");
+    string input;
+    
+    int cursor = 0;
+    while(getline(file, input)) {
+
+        bool validInstruction = false;
+
+        while(!validInstruction) {
+            
+        }
+        
+        
+        
+    }
+
+    int answer = 0; 
+    cout << "\n\nAnswer: " << answer;
+
+    file.close();
+    return 0;
+}
