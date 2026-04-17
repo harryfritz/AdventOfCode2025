9b10aa12fe7dfedf5116792cc65be32fae05157c
harryfritz <mhfritz@gmail.com>
2026-04-17 13:22:37 +0200

    Day8 part one and two solved

--- 2024/Day8/day8_part2.cpp
+++ 2024/Day8/day8_part2.cpp
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////
 //
 // Advent of Code 2024
-// Day 8 - Part one
+// Day 8 - Part two
 // Author: Fritz, M.H.
 //
 //
@@ -15,11 +15,31 @@
 
 using namespace std;
 
-vector <int> getAntinodeCoord(vector <int> ant1, vector <int> ant2) {
-    vector <int> antinode;
-    antinode.push_back(ant2[0] - ant1[0] + ant2[0]);
-    antinode.push_back(ant2[1] - ant1[1] + ant2[1]);
-    return antinode;
+vector <vector <int>> getAntinodesCoord(vector <int> ant1, vector <int> ant2, int rows, int columns) {
+    vector <vector <int>> antinodes;
+    
+    vector <int> director;    
+    director.push_back(ant2[0] - ant1[0]);
+    director.push_back(ant2[1] - ant1[1]);
+    
+    vector <int> nextAntinode;
+    vector <int> prevAntinode = ant1;
+    bool outOfBound = false;
+    while(!outOfBound) {
+        nextAntinode.push_back(prevAntinode[0] + director[0]);
+        nextAntinode.push_back(prevAntinode[1] + director[1]);
+        if(nextAntinode[0] >= 0 && nextAntinode[0] < columns && 
+            nextAntinode[1] >= 0 && nextAntinode[1] < rows) {
+                antinodes.push_back(nextAntinode);
+                prevAntinode = nextAntinode;
+                nextAntinode.clear();
+            } else {
+                outOfBound = true;
+            } 
+        
+    }
+    return antinodes;
+    
 }
 
 int main() {
@@ -45,12 +65,14 @@ int main() {
                     for(int x2 = 0; x2 < antennaMap[y2].size(); x2++) {
                         if(antennaMap[y2][x2] == antennaMap[y1][x1] && x1 != x2 && y1 != y2) {
                             vector <int> ant2 = {x2,y2};
-                            vector <int> antinode = getAntinodeCoord(ant1, ant2);
-                            if(antinode[0] >= 0 && antinode[0] < antinodeMap[0].size() && 
+                            vector <vector <int>> antinodes = getAntinodesCoord(ant1, ant2, antinodeMap.size(), antinodeMap[0].size());
+                            for(vector <int> antinode : antinodes) {
+                                if(antinode[0] >= 0 && antinode[0] < antinodeMap[0].size() && 
                                 antinode[1] >= 0 && antinode[1] < antinodeMap.size() && antinodeMap[antinode[1]][antinode[0]] != '#') {
                                     antinodeMap[antinode[1]][antinode[0]] = '#';
                                     answer++;
                                 }
+                            }
                         }
                     }
                 }
